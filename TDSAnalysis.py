360b43b481cc3176b48d2c69a367eff70dcf735a
swemmerson <33458152+swemmerson@users.noreply.github.com>
2021-04-01 09:42:59 -0500

    Added KDE analysis with statsmodels

--- TDSAnalysis.py
+++ TDSAnalysis.py
@@ -6,13 +6,15 @@ Created on Thu Feb  4 22:47:00 2021
 @author: semmerson
 """
 
-import os, csv
+import os, csv, pyart
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib.patches import Ellipse
 import statsmodels.api as sm
-
+from scipy import stats
+from matplotlib.ticker import FormatStrFormatter
+from matplotlib import animation
 def plot_point_cov(points, nstd=2, ax=None, **kwargs):
     # Wrapper function for drawing a confidence ellipse for a set of points
     pos = points.mean(axis=0)
@@ -46,7 +48,7 @@ def reg_m(y, x):
     return results
 
 # Ugly data import
-os.chdir('data')
+#os.chdir('data')
 data = []
 header = ''
 with open('TDSdata.csv') as f:
@@ -120,31 +122,122 @@ ind4 = TDSdata[:,6] >= 166
 inds = np.vstack((ind0,ind1,ind2,ind3,ind4))
 cols = ['#AFFFAF','#0FFF0F','#FAFA00','#EA0000','#FF00FF']
 
-TDSH = 24
-VR = 85.5
+TDSH = 7.8
+VR = 54.0
+
+X, Y, Z = np.mgrid[0:160:641j, 0:50:201j,50:220:171j]
+positions = np.vstack([X.ravel(), Y.ravel(), Z.ravel()])
+if "vals" not in locals():
+    #kde = stats.gaussian_kde(TDSdata[:,np.array([1,4,6])].T,bw_method=0.38)
+#    vals = np.reshape(kde(positions),X.shape)
+    kde = sm.nonparametric.KDEMultivariate(TDSdata[:,np.array([1,4,6])].T,'ccc',bw=np.array([7.32, 2.54, 4.51]))
+    vals = np.reshape(kde.pdf(positions),X.shape)
+    kss = np.nansum(vals,axis=2)
+    ks = vals/kss[:,:,np.newaxis]
+    exp = np.nansum(ks*np.linspace(50,220,171),axis=2)
+
+expected = exp[int(VR*4),int(TDSH*4)]
+fig = plt.figure(figsize=(12,8))
+ax = fig.add_subplot(111)
+cfs = ax.contourf(X[:,:,0],Y[:,:,0],exp,cmap='RdYlBu_r',levels=np.arange(60,230,10))
+cb = plt.colorbar(cfs,ax=ax)
+cb.set_label('Expected Windspeed (mph)')
+cs = ax.contour(X[:,:,0],Y[:,:,0],exp,levels=np.array([86,111,136,166,201]),colors='black')
+fmt = {}
+strs = ['EF1', 'EF2', 'EF3', 'EF4', 'EF5']
+for l, s in zip(cs.levels, strs):
+    fmt[l] = s
+
+ax.clabel(cs, cs.levels, inline=True, fmt=fmt, fontsize=10)
+ax.set_xlabel('Vrot (kt)')
+ax.set_ylabel('TDS Height (kft)')
+ax.set_title('TDS Height + Vrot Intensity Guidance')
+plt.tight_layout()
+    
+#def animate(i):
+#    VR = 20+0.5*i
+#    TDSH = 0.2*i
+#    plt.clf()
+fig = plt.figure(figsize=(8,5))
+ax = fig.add_subplot(111)
+thresh = 0.15
+p = 3e-3
+x = np.linspace(50,220,171)
+y = vals[int(VR*4),int(TDSH*4),:]
+y = y/np.sum(y)
+ax.plot(x,y,color='black')
+ind = np.where(x == 85)[0][0]
+ind1 = np.where(x == 110)[0][0]
+ax.fill_between(x[:ind+1],y[:ind+1],facecolor=cols[0],alpha=0.5)
+if np.sum(y[:ind+1]) > thresh:
+    xpos = np.mean(x[:ind+1][y[:ind+1] > p])
+    ax.annotate('EF0',(xpos,1e-3),alpha=0.8,fontfamily='Tahoma',fontsize=16,weight='heavy',ha='center',color='#8FCF8F')
+ax.fill_between(x[ind:ind1+1],y[ind:ind1+1],facecolor=cols[1],alpha=0.5)
+if np.sum(y[ind:ind1+1]) > thresh:
+    xpos = np.mean(x[ind:ind1+1][y[ind:ind1+1] > p])
+    ax.annotate('EF1',(xpos,1e-3),alpha=0.8,fontfamily='Tahoma',fontsize=16,weight='heavy',ha='center',color='#0FCF0F')
+ind = ind1
+ind1 = np.where(x == 135)[0][0]
+ax.fill_between(x[ind:ind1+1],y[ind:ind1+1],facecolor=cols[2],alpha=0.5)
+if np.sum(y[ind:ind1+1]) > thresh:
+    xpos = np.mean(x[ind:ind1+1][y[ind:ind1+1] > p])
+    ax.annotate('EF2',(xpos,1e-3),alpha=0.8,fontfamily='Tahoma',fontsize=16,weight='heavy',ha='center',color='#CACA00')
+ind = ind1
+ind1 = np.where(x == 165)[0][0]
+ax.fill_between(x[ind:ind1+1],y[ind:ind1+1],facecolor=cols[3],alpha=0.5)
+if np.sum(y[ind:ind1+1]) > thresh:
+    xpos = np.mean(x[ind:ind1+1][y[ind:ind1+1] > p])
+    ax.annotate('EF3',(xpos,1e-3),alpha=0.8,fontfamily='Tahoma',fontsize=16,weight='heavy',ha='center',color=cols[3])
+ind = ind1
+ind1 = np.where(x == 200)[0][0]
+ax.fill_between(x[ind:ind1+1],y[ind:ind1+1],facecolor=cols[4],alpha=0.5)
+if np.sum(y[ind:ind1+1]) > thresh:
+    xpos = np.mean(x[ind:ind1+1][y[ind:ind1+1] > p])
+    ax.annotate('EF4',(xpos,1e-3),alpha=0.8,fontfamily='Tahoma',fontsize=16,weight='heavy',ha='center',color=cols[4])
+ind = ind1
+ind1 = np.where(x == 220)[0][0]
+ax.fill_between(x[ind:ind1],y[ind:ind1],facecolor='#9B2FFF',alpha=0.5)
+if np.sum(y[ind:ind1+1]) > thresh:
+    xpos = np.mean(x[ind:ind1+1][y[ind:ind1+1] > p])
+    ax.annotate('EF5',(xpos,1e-3),alpha=0.8,fontfamily='Tahoma',fontsize=16,weight='heavy',ha='center',color='#7B0FDF')
+ax.ticklabel_format(axis='y',style='sci',scilimits=(0,0))
+ax.set_ylim(0,5e-2)
+ax.set_title(f'Intensity PDF for {np.round(TDSH,1)} kft TDS + {VR} kt Vrot')
+ax.set_xlabel('Peak Wind Speed (mph)')
+ax.set_ylabel('Probability Density')
+plt.tight_layout()
+plt.savefig('TDSpdf.png')
+#fig = plt.figure(figsize=(8,5))
+#anim = animation.FuncAnimation(fig, animate, frames=200, interval=20)
+#anim.save('basic_animation.gif', fps=20)
+
 
 # Ellipse plotting part
-g = sns.JointGrid(height=7,xlim=(0,160),ylim=(0,40))
+g = sns.JointGrid(TDSdata[:,1],TDSdata[:,4],height=7,xlim=(0,160),ylim=(0,40))
 ellipses = []
 for i in range(5):
     fd = TDSdata[inds[i,:],:]
     e = plot_point_cov(fd[:,np.array([1,4])],nstd=2,ax=g.ax_joint,alpha=0.3,color=cols[i],lw=1.5)
-    sns.kdeplot(x=fd[:,1],ax=g.ax_marg_x,common_norm=False,color=cols[i],fill=True,cut=0)
-    sns.kdeplot(y=fd[:,4],ax=g.ax_marg_y,common_norm=False,color=cols[i],fill=True,cut=0)
+    sns.kdeplot(fd[:,1],ax=g.ax_marg_x,color=cols[i],shade=True,cut=0)
+    sns.kdeplot(fd[:,4],ax=g.ax_marg_y,color=cols[i],vertical=True,shade=True,cut=0)
     ellipses.append(e)
 g.ax_joint.set_xlabel('Vrot (kt)')
 g.ax_joint.set_ylabel('TDS Height (kft)')
 g.ax_joint.legend(ellipses,['EF0','EF1','EF2','EF3','EF4+'])
-g.ax_joint.scatter(x=VR,y=TDSH,c="#F7F7F7",marker="v",s=75,edgecolors='black')
-g.ax_joint.annotate('Longwood, NC EFU',(VR+3,TDSH-0.4))
+g.ax_joint.scatter(x=VR,y=TDSH,c='#AFAFAF',marker="v",s=75,edgecolors='black',zorder=2)
+g.ax_joint.annotate('Nolan Place, LA EFU',(VR+3,TDSH-0.6),fontsize=13,ha='left')
 g.ax_joint.annotate('Sam Emmerson\n@ou_sams',(130,2),alpha=0.3,fontfamily='Tahoma',fontsize=16,weight='heavy',ha='center')
 plt.tight_layout()
+fig = plt.gcf()
+fig.set_figheight(7)
+fig.set_figwidth(11)
+plt.savefig('TDSH.png')
 
-obsv = np.array([VR,TDSH])
+obsv = np.array([VR,TDSH,expected])
 
 # Analog finder
-windows = np.array([5,2])
-analogs = np.all(np.logical_and(TDSdata[:,np.array([1,4])] > obsv-windows,TDSdata[:,np.array([1,4])] < obsv+windows),axis=1)
+windows = np.array([5,2,30])
+analogs = np.all(np.logical_and(TDSdata[:,np.array([1,4,6])] > obsv-windows,TDSdata[:,np.array([1,4,6])] < obsv+windows),axis=1)
 print(f"\n TDS Analogs for a TDS height of {TDSH} kft and a Vrot of {VR} kt:")
 print(f" Number of Analogs: {np.sum(analogs)} \n")
 print(f" % EF0:  {np.round(100*np.sum(TDSdata[analogs,:][:,6] < 86)/np.sum(analogs),1)}%")
@@ -159,7 +252,7 @@ print(f" {np.min(TDSdata[analogs,6])} \
       {np.round(np.mean(TDSdata[analogs,6]),1)} \
       {np.round(np.percentile(TDSdata[analogs,6],75),1)} \
       {np.max(TDSdata[analogs,6])}\n")
-scores = np.sum(((TDSdata[analogs,:][:,np.array([1,4])] - obsv)**2)*np.array([0.45,0.55]),axis=1)
+scores = np.sum((((TDSdata[analogs,:][:,np.array([1,4,6])] - obsv))*np.array([0.45,0.5,0.05]))**2,axis=1)
 sinds = np.argsort(scores)
 ana_inds = np.where(analogs)[0]
 ana_inds = ana_inds[sinds]
@@ -176,16 +269,27 @@ for i in range(n):
     tag = ana_tags[i]
     if TDSdata[ana_inds[i],6] < 86:
         ef = 'EF0'
+        col = cols[0]
     elif TDSdata[ana_inds[i],6] < 111:
         ef = 'EF1'
+        col = cols[1]
     elif TDSdata[ana_inds[i],6] < 136:
         ef = 'EF2'
+        col = cols[2]
     elif TDSdata[ana_inds[i],6] < 166:
         ef = 'EF3'
+        col = cols[3]
     elif TDSdata[ana_inds[i],6] < 201:
         ef = 'EF4'
+        col = cols[4]
     else:
         ef = 'EF5'
     print(f" #{i+1}: {int(TDSdata[ana_inds[i],6])} mph {ef} {tag[5]} County, {tag[4]} {tag[1]}/{tag[2]}/{tag[0]} @{str.rjust(tag[3],4,'0')}z")
-    
-#lm = reg_m(TDSdata[:,6],TDSdata[:,3:5].T)
+#    VR = TDSdata[ana_inds[i],1]
+#    TDSH = TDSdata[ana_inds[i],4]
+#    g.ax_joint.scatter(x=VR,y=TDSH,c=col,marker="v",s=75,edgecolors='black',zorder=2)
+#    g.ax_joint.annotate(f'{tag[5]} County, {tag[4]} {ef}',(VR+3,TDSH-0.6),fontsize=13,ha='left')
+
+
+
+
